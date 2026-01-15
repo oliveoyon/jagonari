db9baaff67440d6130a841b93cc8fb3c9fb3cf51
oliveoyon <arifur123@gmail.com>
2026-01-15 11:51:36 +0600

    test

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,12 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return 'Storage symlink created successfully!';
+});
 
 Route::get('/', [WebController::class, 'home']);
 Route::get('/contact', [WebController::class, 'contact']);
@@ -30,7 +33,7 @@ Route::post('/jobs/{slug}/apply', [WebController::class, 'applyJob'])->name('web
 
 // Dynamic page route (catch-all) - keep at the bottom
 Route::get('/{slug}', [WebController::class, 'showPage'])
-    ->where('slug', '^(?!login|register|password|dashboard|admin|contact|jobs).*$')
+    ->where('slug', '^(?!login|register|password|dashboard|admin|storage-link|contact|jobs).*$')
     ->name('dynamic.page');
 
 
